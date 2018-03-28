eb220fd8d5dd8aa3d8c3796a8ee0557f8ebfbbe7
Pedro Rodriguez <entilzha@quenya.umiacs.umd.edu>
2018-03-28 17:21:50 -0400

    Finalize code

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,20 @@
 extern crate clap;
 extern crate serde;
 extern crate serde_json;
+extern crate rayon;
 
 #[macro_use]
 extern crate serde_derive;
 
+use std::fmt::format;
 use std::fs::File;
 use std::io::{BufReader, BufRead};
 use std::vec::Vec;
-use std::iter;
 use std::iter::Iterator;
+use std::path::Path;
+use std::io::prelude::*;
 use clap::{Arg, App};
+use rayon::prelude::*;
 
 
 use serde_json::Value;
@@ -19,58 +23,88 @@ use serde_json::map::{Map, Values};
 #[derive(Deserialize)]
 struct Item {
     id: String,
-    #[serde(rename="type")]
-    wd_type: String,
     claims: Map<String, Value>,
+    #[serde(default)]
     sitelinks: Value
 }
 
-#[derive(Debug)]
+#[derive(Debug, Serialize)]
 struct Relation {
     subject: String,
     property: String,
-    value: String
+    value: String,
+    title: Option<String>
 }
 
 
-fn extract_claims(subject_id: &String, claim_snaks: Values) -> Vec<Relation> {
+fn parse_value(datatype: &str, mainsnak: &Value) -> Option<String> {
+    match datatype {
+        "monolingualtext" => {
+            mainsnak["datavalue"]["value"]["text"].as_str().map(|s| s.to_string())
+        },
+        "wikibase-item" | "wikibase-property" => {
+            mainsnak["datavalue"]["value"]["id"].as_str().map(|s| s.to_string())
+        }
+        "external-id" | "url" | "globe-coordinate" | "quantity"
+            | "string" | "commonsMedia" | "time" | "math" => None,
+        _ => {
+            panic!("Oops, unhandled data type")
+        }
+    }
+}
+
+
+fn extract_claims(subject_id: &String, maybe_title: Option<&str>, claim_snaks: Values) -> Vec<Relation> {
     claim_snaks.flat_map(|raw_snak_values| {
         let snak_values = raw_snak_values.as_array().unwrap();
         snak_values.iter().flat_map(|snak| {
             let mainsnak = &snak["mainsnak"];
             let subject = subject_id.clone();
-            let datatype = mainsnak["datatype"].as_str().expect("Expected datatype to exist");
-            let property = mainsnak["property"]
-                .as_str()
-                .expect("Expected property to exist")
-                .to_string();
-            match datatype {
-                "monolingualtext" => {
-                    println!("{}", mainsnak["datavalue"]["value"]);
-                    let value = mainsnak["datavalue"]["value"]["text"]
+            match mainsnak["datatype"].as_str() {
+                Some(datatype) => {
+                    let property = mainsnak["property"]
                         .as_str()
-                        .expect("Expected text value to exist")
+                        .expect("Expected property to exist")
                         .to_string();
-                    Some(Relation {subject, property, value})
-                },
-                "wikibase-item" | "wikibase-property" => {
-                    match mainsnak["datavalue"]["value"]["id"].as_str() {
-                        Some(str_value) => {
-                            let value = str_value.to_string();
-                            Some(Relation {subject, property, value})
-                        },
+                    let maybe_value = parse_value(datatype, mainsnak);
+                    let title = maybe_title.map(|s| s.to_string());
+                    match maybe_value {
+                        Some(value) => Some(Relation {subject, property, value, title}),
                         None => None
                     }
-                }
-                "external-id" | "url" | "globe-coordinate" | "quantity"
-                    | "string" | "commonsMedia" | "time" | "math" => None,
-                _ => {
-                    println!("\n!!!!Skipping: {}", datatype);
-                    println!("!!!!Claim: {}", mainsnak);
-                    None
-                }
+                },
+                None => None
             }
         })
+    }).collect()
+}
+
+fn line_to_relations(line: &str) -> Vec<Relation> {
+    if line.starts_with("[") || line.starts_with("]") {
+        Vec::new()
+    } else {
+        let valid_json = match line.chars().last() {
+            Some(',') => {
+                let (valid_json, _) = line.split_at(line.len() - 1);
+                valid_json
+            },
+            _ => line
+        };
+        let wd_item: Item = serde_json::from_str(valid_json)
+            .expect(format!("Valid json not found: {}", valid_json).as_str());
+        let id = &wd_item.id;
+        let title = wd_item.sitelinks["enwiki"]["title"].as_str();
+        extract_claims(id, title, wd_item.claims.values())
+    }
+}
+
+fn filename_to_relations(filename: &str) -> Vec<Relation> {
+    let file = File::open(filename).expect("file not found");
+    BufReader::new(file).lines().flat_map(|l| {
+        match l {
+            Ok(l) => line_to_relations(&l),
+            Err(_) => Vec::new()
+        }
     }).collect()
 }
 
@@ -84,26 +118,14 @@ fn main() {
                               Arg::with_name("INPUT")
                               .help("Location of wikidata json dump")
                               .required(true)
-                              .index(1))
+                              .index(1)
+                              .multiple(true))
                           .get_matches();
-    let input_filename = matches.value_of("INPUT").unwrap();
-    let mut file = File::open(input_filename).expect("file not found");
-    for raw_line in BufReader::new(file).lines() {
-        match raw_line {
-            Ok(line) => {
-                if line.starts_with("[") || line.starts_with("]"){
-                    println!("Skipping header");
-                } else {
-                    let length = line.len();
-                    let (valid_json, _) = line.split_at(length - 1);
-                    let wd_item: Item = serde_json::from_str(valid_json).expect("Valid json not found");
-                    let id = &wd_item.id;
-                    println!("{:?}", extract_claims(id, wd_item.claims.values()));
-
-                    println!("{}", id);
-                }
-            },
-            _ => ()
-        }
-    }
+    let input_files: Vec<&str> = matches.values_of("INPUT").unwrap().collect();
+    input_files
+        .par_iter()
+        .flat_map(|f| filename_to_relations(f))
+        .for_each(|r| {
+            println!("{}", serde_json::to_string(&r).unwrap());
+        })
 }
